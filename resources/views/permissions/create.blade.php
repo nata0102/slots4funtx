3a10c04dcc2878ecaef648de8ed9f85d00957c21
Carlos Espinosa <carlos.espinosa@sitilab.com>
2021-11-25 02:07:29 -0600

    Permisos con QR

--- resources/views/permissions/create.blade.php
+++ resources/views/permissions/create.blade.php
@@ -10,13 +10,19 @@
 
         <a href="{{url()->previous()}}" class="btn btn-info" style="width: 40px; margin-bottom: 10px"><i class="fas fa-long-arrow-alt-left"></i></a>
 
-        <button  id="boton_qr" hidden class="btn btn-info" style="width: 40px; height: 40px; margin-bottom: 10px;margin-left: 350px" onclick="readQR()"><i class="fas fa-qrcode"></i></button>
-
-        <div style="margin-left: 200px;" id="div_cam" class="col-md-6" hidden>
-          <video id="preview" width="100%"></video>
-          <input type="text" name="text" id="text_qr">
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="form-group">
+                <button  id="boton_qr" hidden class="btn btn-info" style="width: 40px; height: 40px; margin-bottom: 10px;" onclick="readQR()"><i class="fas fa-qrcode"></i></button>
+            </div>
+          </div>
+          <div id="div_cam" class="col-12 col-sm-6 col-md-4" hidden>
+            <div class="form-group">        
+              <video id="preview" width="100%"></video>
+              <input type="text" name="text" id="text_qr">
+            </div>
+          </div>
         </div>
-
         <form class="" action="{{action('PermissionController@store')}}" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
             @csrf           
             <div class="row">
