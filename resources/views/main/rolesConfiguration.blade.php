989da6c61a5a8762cd10a85e0dca28c289807871
Natanael <nata@MacBook-Pro-de-Natanael.local>
2022-04-08 18:47:40 -0500

    roles

--- resources/views/main/rolesConfiguration.blade.php
+++ resources/views/main/rolesConfiguration.blade.php
@@ -33,16 +33,16 @@
               <input type="hidden" value="{{$role->id}}" name="role[{{$r}}]">
 
               <input type="hidden" name="read[{{$r}}]" value="0">
-              <input value="1" type="checkbox" name="read[{{$r}}]" {{str_contains($mr->actions, 'R') ? 'checked' : ''}} style="position: absolute; left: 160px; top: 5px;" id="{{$r}}-read" ><label for="{{$r}}-read" style="position: absolute; left: 175px; top: 0; margin: 0;">Read</label>
+              <input value="1" type="checkbox" name="read[{{$r}}]" {{ $mr ? str_contains($mr->actions, 'R') ? 'checked' : '' : ''}} style="position: absolute; left: 160px; top: 5px;" id="{{$r}}-read" ><label for="{{$r}}-read" style="position: absolute; left: 175px; top: 0; margin: 0;">Read</label>
 
               <input type="hidden" name="create[{{$r}}]" value="0">
-              <input value="1" type="checkbox" name="create[{{$r}}]" {{str_contains($mr->actions, 'C') ? 'checked' : ''}} style="position: absolute; left: 260px; top: 5px;" id="{{$r}}-create" ><label for="{{$r}}-create" style="position: absolute; left: 275px; top: 0; margin: 0;">Create</label>
+              <input value="1" type="checkbox" name="create[{{$r}}]" {{ $mr ? str_contains($mr->actions, 'C') ? 'checked' : '' : ''}} style="position: absolute; left: 260px; top: 5px;" id="{{$r}}-create" ><label for="{{$r}}-create" style="position: absolute; left: 275px; top: 0; margin: 0;">Create</label>
 
               <input type="hidden" name="update[{{$r}}]" value="0">
-              <input value="1" type="checkbox" name="update[{{$r}}]" {{str_contains($mr->actions, 'U') ? 'checked' : ''}} style="position: absolute; left: 360px; top: 5px;" id="{{$r}}-update" ><label for="{{$r}}-update" style="position: absolute; left: 375px; top: 0; margin: 0;">Update</label>
+              <input value="1" type="checkbox" name="update[{{$r}}]" {{ $mr ? str_contains($mr->actions, 'U') ? 'checked' : '' : ''}} style="position: absolute; left: 360px; top: 5px;" id="{{$r}}-update" ><label for="{{$r}}-update" style="position: absolute; left: 375px; top: 0; margin: 0;">Update</label>
 
               <input type="hidden" name="delete[{{$r}}]" value="0">
-              <input value="1" type="checkbox" name="delete[{{$r}}]" {{str_contains($mr->actions, 'D') ? 'checked' : ''}} style="position: absolute; left: 460px; top: 5px;" id="{{$r}}-delete" ><label for="{{$r}}-delete" style="position: absolute; left: 475px; top: 0; margin: 0;">Delete</label>
+              <input value="1" type="checkbox" name="delete[{{$r}}]" {{ $mr ? str_contains($mr->actions, 'D') ? 'checked' : '' : ''}} style="position: absolute; left: 460px; top: 5px;" id="{{$r}}-delete" ><label for="{{$r}}-delete" style="position: absolute; left: 475px; top: 0; margin: 0;">Delete</label>
 
             </div>
             <hr>
