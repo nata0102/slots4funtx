6a4cc11b651ef2910f218fb2d910305cf2e67757
Rodrigo Natanael <rodrigo.pescina.91@gmail.com>
2022-10-13 18:08:45 -0500

    idioma

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 */
 
+App::setLocale("en");
+
   Route::get('/',['as'=>'login','uses'=>'MainController@index']);
   Route::post('/login', 'MainController@login');
   Route::post('/logout', 'MainController@logout');
